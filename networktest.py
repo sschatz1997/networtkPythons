8aae2359bd4c65dfa16b202fd70c2498b08d1035
sschatz1997 <s.schatz1997@gmail.com>
2018-02-17 11:07:52 -0500

    Update networktest.py

--- networktest.py
+++ networktest.py
@@ -2,20 +2,41 @@ import time
 from time import sleep
 import os
 
-os.system("ping -c 3 8.8.8.8 > pingresults.txt")
+linuxV = "DISTRO"#this is changed by setup.py
 
+if linuxV == "ubuntu":#they both use ifconfig
+	linuxV = "debian"
+if linuxV == "debian9": 
+	ipset = 'D9'
+else:
+	ipset = 'ifconfig eth0 >> /test/infconfig.txt'
+
+os.system("sudo ping -c 3 8.8.8.8 > pingresults.txt")
+
+
+os.system("ifconfig eth0  > /test/ifconfig.txt")	
+config = os.stat('/test/ifconfig.txt')
+counter = 0 
 time.sleep(15)
-statinfo = os.stat('pingresults.txt')
+statinfo = os.stat('/test/pingresults.txt')
 state = 'false'
-		
 
-while state  != 'true':
+while (state  != 'true') and (counter < 3):
 	if  statinfo.st_size < 200:
 		print("network is down")
 		state = 'false'
-		os.system("service network restart")
+		os.system("sudo service networking restart")
 		time.sleep(2)
+		counter += 1
+		
 	else:
 		print("network is up")
 		state = 'true'
+		
+	if counter == 2 and config.st_size <= 354:
+		print("No ip address assigned")
 
+	if counter == 3:
+		print("network can't be restarted")	 
+
+os.system("date >> pingresults.txt")
